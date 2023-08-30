0b3d6f418e497ea8ae4bba0125b29eb238950028
Thor Whalen <thorwhalen1@gmail.com>
2023-08-30 19:21:00 +0200

    feat: replace_ids_in_code

--- jy/js_parse.py
+++ jy/js_parse.py
@@ -50,19 +50,19 @@ def parse_js_code(js_code: str, encoding: Optional[str] = None) -> AstScript:
 
 @if_none_output_raise_unknown_type("to extract obj name from")
 def _extract_obj_name(x: AstNode, /):
-    if x.type == 'Identifier':
+    if x.type == "Identifier":
         return x.name
-    elif x.type == 'MemberExpression':
+    elif x.type == "MemberExpression":
         object_name = _extract_obj_name(x.object)
         property_name = _extract_obj_name(x.property)
-        return f'{object_name}.{property_name}'
+        return f"{object_name}.{property_name}"
 
 
 @if_none_output_raise_unknown_type("to extract params from")
 def _extract_params(x: AstNode, /):
-    if x.type == 'Identifier':
+    if x.type == "Identifier":
         return dict(name=x.name)
-    elif x.type == 'AssignmentPattern':
+    elif x.type == "AssignmentPattern":
         return dict(name=x.left.name, default=x.right.value)
 
 
@@ -77,21 +77,21 @@ def _extract_params_from_function_expression(x):
 def extract_func_name_and_params(ast_node: AstNode):
     """Extract one or several function ``(name, params)`` pair(s) from an AST node"""
     x = ast_node
-    if x.type == 'FunctionDeclaration':
+    if x.type == "FunctionDeclaration":
         yield x.id.name, list(extract_js_func_params(x.params))
-    elif x.type == 'AssignmentExpression':
+    elif x.type == "AssignmentExpression":
         yield (
             _extract_obj_name(x.left),
             list(extract_js_func_params(x.right.params)),
         )
-    elif x.type == 'VariableDeclarator':
+    elif x.type == "VariableDeclarator":
         yield x.id.name, list(extract_js_func_params(x.init.params))
-    elif x.type == 'VariableDeclaration':
+    elif x.type == "VariableDeclaration":
         # Here we may have several declarations, so we use yield from
         yield from chain.from_iterable(
             map(extract_func_name_and_params, x.declarations)
         )
-    elif x.type == 'ExpressionStatement':
+    elif x.type == "ExpressionStatement":
         yield from extract_func_name_and_params(x.expression)
 
 
@@ -141,3 +141,54 @@ def dflt_py_to_js_value_trans(x):
     elif isinstance(x, dict):
         return json.dumps(x)
     return x
+
+
+import re
+import uuid
+from typing import Tuple
+
+_dflt_patterns_for_html_ids = (
+    r'id="([^"]+)"',  # HTML id attributes
+    r"#([a-zA-Z][\w\-]*)",  # CSS IDs
+    r'document\.getElementById\("([^"]+)"\)',  # JS getElementById
+)
+
+
+def replace_ids_in_code(
+    code: str, patterns: Tuple[str] = _dflt_patterns_for_html_ids
+) -> (str, dict):
+    """
+    Replaces IDs in a mixed HTML, CSS, and JS string with unique versions.
+
+    Args:
+    - code (str): The input code containing HTML, CSS, and/or JS.
+    - patterns (tuple): A tuple of regex patterns to match IDs.
+
+    Returns:
+    - tuple: A tuple containing the modified code and a dictionary of old-to-new ID mappings.
+
+    This function is useful to make id-unique copies of HTML/JS components that have IDs.
+    This need was noticed and discussed in: https://github.com/i2mint/jy/discussions/3#discussioncomment-6862843
+    
+    """
+    # Patterns to match IDs
+    patterns = [
+        r'id="([^"]+)"',  # HTML id attributes
+        r"#([a-zA-Z][\w\-]*)",  # CSS IDs
+        r'document\.getElementById\("([^"]+)"\)',  # JS getElementById
+    ]
+
+    replaced = {}
+
+    # Generate a unique ID
+    def unique_id(match):
+        old_id = match.group(1)
+        new_id = old_id + "_" + str(uuid.uuid4()).replace("-", "")[:8]
+        replaced[old_id] = new_id
+        return match.group(0).replace(old_id, new_id)
+
+    # Replace all IDs in the code
+    for pattern in patterns:
+        code = re.sub(pattern, unique_id, code)
+
+    return code, replaced
